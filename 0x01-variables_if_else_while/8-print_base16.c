7ca440463559a886869624c4d3f771fbc81b25e9
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-10 09:17:11 -0700

    Task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@ int main(void)
 char n;
 for (n = '0'; n <= '9'; n++)
 {
-putchar(n);    
+putchar(n);
 }
 for (n = 'a'; n < 'g'; n++)
 {
-putchar(n);    
+putchar(n);
 }
-putchar("\n");
+putchar('\n');
+return (0);
 }
