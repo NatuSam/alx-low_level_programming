6408fecfdf5440aa97a4b7d98cf0fe6601396021
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 07:36:21 -0700

    TAsk6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - return abs of n
+ * @n: variable to determine if input is lowercase
+ * Return: Always x (Success)
+ */
+int _abs(int n)
+{
+int x;
+x = abs(n);
+return (x);
+}
