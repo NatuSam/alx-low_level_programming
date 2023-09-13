17298ed1ac1b514a5d9dcd20c75afd13f7cafd49
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-12 22:16:25 -0700

    Task2

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - return a-z x10
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+char n;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+return (0);
+}
