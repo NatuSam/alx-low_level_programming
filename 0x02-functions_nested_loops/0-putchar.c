63a7af26b0d9e2cde6e123880faf4da7cb837674
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 06:56:16 -0700

    TAsk0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-int n;
-char put[8] = ('_', 'p', 'u', 't', 'c', 'h','a', 'r');
-for (n = 0; n < 8; n++)
-{
-_putchar(put[n]);
-}
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
