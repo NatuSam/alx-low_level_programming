e2b4dcdd73d950a8d1a150c454587ba5764ffedb
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 06:45:39 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("_putchar");
+_putchar("_putchar");
 return (0);
 }
