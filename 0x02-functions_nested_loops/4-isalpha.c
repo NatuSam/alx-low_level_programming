d3b9e920b54db59a12be5f720afd5a26ea543bff
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 07:23:13 -0700

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - return 1 or 0
+ * @c: variable to determine if input is lowercase
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+int l = isalpha(c);
+if (l > 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
