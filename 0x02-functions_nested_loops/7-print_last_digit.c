6265326b531c6ca6c5d0bf5748c79da63ef09e35
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 07:47:07 -0700

    prototypes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - return 1 or 0
+ * @n: variable to determine if input is lowercase
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+n = abs(n) % 10;
+return (n);
+}
