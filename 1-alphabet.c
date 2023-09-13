ff9c4738048ea599753d74c3de47113f6647bbeb
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-12 22:13:40 -0700

    Task1

---
+++ 1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - return a-z
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+return (0);
+}
