7f4c7ab56738b42ef10227d64dd6ae48f912c898
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-13 07:29:56 -0700

    Task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_sign - return 1 or 0
+ * @c: variable to determine if input is lowercase
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
