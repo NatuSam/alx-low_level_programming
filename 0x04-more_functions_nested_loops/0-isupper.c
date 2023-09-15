2c8b049bba3ea6a212ede06065e578fcb17b80e1
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 00:54:01 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype>
+
+/*
+ *_isupper - return 1 if c is uppercase
+ *@c: is parameter
+ * Return: Always 0 (Success)
+ */
+int _issupper(int c)
+{
+if (isupper(c) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
