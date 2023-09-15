27399e972135328e6d8076db08409e16fd4d8e6a
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 05:53:25 -0700

    Task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_diagonal - display spaces and enter
+ *@n: is the num of space
+ */
+void print_diagonal(int n)
+{
+int x;
+int i;
+if (n == 0)
+_putchar(\n);
+else
+{
+for  (x = 1; x <= n; x++)
+{
+for (i = 1; i <= x; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n';
+}
+}
+}
