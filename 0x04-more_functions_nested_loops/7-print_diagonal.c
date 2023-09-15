a85ed40044494e1836c61df6447b6139e14e91b0
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 06:00:35 -0700

    Task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,21 @@
 #include "main.h"
+
 /**
- *print_diagonal - display spaces and enter
- *@n: is the num of space
+ * print_diagonal - print a straight line using putchar of n size
+ * @n: size of line
  */
+
 void print_diagonal(int n)
 {
 int x;
-int i;
+int y;
 if (n <= 0)
 _putchar('\n');
-else
+for (y = 0; y < n; y++)
 {
-for (x = 1; x <= n; x++)
-{
-for (i = 1; i <= x; i++)
-{
+for (x = y; x > 0; x--)
 _putchar(' ');
-}
 _putchar('\\');
 _putchar('\n');
-}
 }
 }
