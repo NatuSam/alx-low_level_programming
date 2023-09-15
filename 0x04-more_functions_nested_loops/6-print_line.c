49d2cbfb2ea79ede1bdf76d609cb3d314fa169d3
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 05:43:03 -0700

    Task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints lines
+ *@n: number of lines
+ */
+void print_line(int n)
+{
+int x;
+for (x = 1; x <= n; x++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
