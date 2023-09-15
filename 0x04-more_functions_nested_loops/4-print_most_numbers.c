c6ad3f45db17892abf0d5ce8d4f38ec988b88974
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 05:05:19 -0700

    Task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - return 1-9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+if (n != '2' || n!= '4')
+{
+_putchar(n);
+}
+}
+_putchar('\n');
+}
