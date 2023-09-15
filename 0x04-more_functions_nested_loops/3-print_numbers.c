66e1ca1e37d82d68d13627740cce6ef675327898
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-15 01:13:55 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - return 1-9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
