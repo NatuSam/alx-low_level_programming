d5eba254c6cf90177b081c6f75d6d58427b3b191
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-18 16:45:59 -0700

    Task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src 
+ *@dest:a pointer to a string
+ *@src: a pointer
+ *Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len, count;
+len = 0;
+while (src[len] != '\0')
+len++;
+for (count = 0; count <= len; count++)
+dest[count] = src[count];
+return (dest);
+}
