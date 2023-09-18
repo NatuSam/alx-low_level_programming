a9fc884d6dee7450ff6693627470f69d6081ce8c
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-18 16:40:43 -0700

    Task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array -  prints n elements of an array
+ *@a:a pointer to a string
+ *@n:length of array
+ */
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (i != (n - 1))
+printf("%i,", a[i]);
+else
+printf("%i", a[i]);
+i++;
+}
+printf("\n");
+}
