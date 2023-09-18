177df8f1ca0605c9ed9abd58064980626ea78a50
NatuSam <natnaelsamsonkebede@gmail.com>
2023-09-18 15:45:42 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - updates the value it points to to 98
+ *@s:a pointer to a string
+ * Return: len of s
+ */
+int _strlen(char *s)
+{
+int x;
+x = srtlen(*s);
+return (x);
+}
