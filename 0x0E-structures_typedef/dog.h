d8e5adf6b46c98b8490194564568ddcfe4a79927
NatuSam <natnaelsamsonkebede@gmail.com>
2023-10-09 08:25:15 -0700

    Task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a new type struct dog
+ */
+struct dog{
+char *name;
+float age;
+char *owner;
+}
