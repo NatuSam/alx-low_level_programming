9f59d87a16b660886259419817f65338597d79b4
NatuSam <natnaelsamsonkebede@gmail.com>
2023-10-19 03:19:25 -0700

    bettyErr

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 #include <stddef.h>
 /**
- * struct list_s - singly linked list
+ * struct list - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -10,11 +10,12 @@
  * Description: singly linked list node str
  */
 
-typedef struct list {
+typedef struct list
+{
 char *str;
 int len;
 struct list *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
